c81d7e46425ca950acfcfbdf77cdcfa6470a8125
BradleyOzO <bradley.1023.chang@gmail.com>
2023-06-20 22:09:22 +0800

    add submission version

---
+++ src/policy/alpha-beta_submission.cpp
@@ -0,0 +1,101 @@
+#include <cstdlib>
+#include <bits/stdc++.h>
+#include <queue>
+#include "../state/state.hpp"
+#include "./alpha-beta.hpp"
+
+
+/**
+* @brief Randomly get a legal action
+* 
+* @param state Now state
+* @param depth You may need this for other policy
+* @return Move 
+*/
+
+using namespace std;
+
+static int cur_player;
+
+int AlphaBeta::alpha_beta_value(State *state, int depth, int alpha, int beta, bool maximizingPlayer){
+    
+    state->get_legal_actions();
+
+    if(state->game_state == WIN){
+        if(state->player == cur_player){
+            return 50000;
+        }
+        else{
+            return -50000;
+        }
+    }
+    
+    if(!depth){
+        if(cur_player==0){
+            return state->evaluate();
+        }
+        else{
+            return (-1)*state->evaluate();
+        }
+    }
+
+
+    auto actions = state->legal_actions;
+    int size = actions.size();
+    int value;
+
+    if(maximizingPlayer){
+        value = -100000;
+        for(int i=0;i<size;i++){
+            int vv = alpha_beta_value(state->next_state(actions[i]), depth-1, alpha, beta, false);
+            if(vv > value){
+                value = vv;
+            }
+            if(value > alpha){
+                alpha = value;
+            }
+            if(alpha >= beta){
+                break;
+            }
+        }
+        return value;
+    }
+
+    else {
+        value = 100000;
+        
+        for(int i=0;i<size;i++){
+            int vv = alpha_beta_value(state->next_state(actions[i]), depth-1, alpha, beta, true);
+            if(vv < value){
+                value = vv;
+            }
+            if(value < beta){
+                beta = value;
+            }
+            if(beta <= alpha){
+                break;
+            }
+        }
+        return value;
+    }
+}
+
+Move AlphaBeta::get_move(State *state, int depth){
+    int value = -1000000;
+    cur_player = state->player;
+    Move result;
+    state->get_legal_actions();
+    auto actions = state->legal_actions;
+    int size = state->legal_actions.size();
+    int brch;
+
+    for(int i=0;i<size;i++){
+        brch = alpha_beta_value(state->next_state(actions[i]), depth, INT_MIN, INT_MAX, false);
+        if(brch > value){
+            value = brch;
+            result = actions[i];
+        }
+    }
+    
+    return result;
+}
